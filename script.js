1b951e40cc1e2ce668a7359bc5577b1803749a94
amandaPerez <chihulloli123@gmail.com>
2025-09-06 11:46:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+function geUrl() {
+let baseUrl = "https://api.github.com/search/repositories";
+
+let params = {
+  q: "language:javascript stars:>100000",
+  per page: 20,
+  sort: "stars"
+};
+
+let queryString = Object.entries(params).map(pair => {
+  return `${pair[0]}=${encodeURIComponent(pair[1])};
+  }).join("&");
+
+  return `$[baseUrl}?${queryString}`;
+  }
+
+  let url = getUrl();
+  console.log(url);
